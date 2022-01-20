d68c2976a801c4f55d6cf403b32f4b0077befd1f
Julia Moskvita <ymoskvita@gmail.com>
2022-01-20 12:44:09 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
 import React, { Component } from 'react';
 import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import Searchbar from './Components/Searchbar';
 import ImageGallery from './Components/ImageGallery';
 import { Container } from './Components/ImagesFinder.styled';
+import imagesApi from './services/images-api'
+import Modal from './Components/Modal';
+import Button from './Components/Button';
+import { StyledLoader } from './Components/ImagesFinder.styled';
 
 export default class App extends Component {
   state = {
     searchQuery: '',
+    showModal: false,
+    selectedImage: null,
+    images: null,
+    error: null,
+    page: 1,
+    status: 'idle',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.setState({ page: 1, status: 'pending' });
+
+      imagesApi
+        .fetchImages(this.state.searchQuery, this.state.page)
+        .then(images => {
+          if (!images.hits.length) {
+            this.setState({ status: 'idle', images: null});
+            return toast.error(`No matches found`)
+          }
+          this.setState({ images: images.hits, status: 'resolved', page: this.state.page + 1 })
+        })
+        .catch(error => this.setState({ error, status: 'rejected'}))
+    }
+  }
 
   handleFormSubmit = searchQuery => {
     this.setState({ searchQuery });
   };
 
+  handleImageClick = link => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal
+    }));
+    this.setState({ selectedImage: link });
+  };
+
+  handleLoadMoreBnt = () => {
+    this.setState({ page: this.state.page + 1 });
+
+    imagesApi
+      .fetchImages(this.state.searchQuery, this.state.page)
+      .then(images => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images.hits],
+        }));
+      })
+  }
+
   render() {
+    const {showModal, selectedImage, images, status, error } = this.state;
+
     return (
       <Container>
         <Toaster />
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchQuery={this.state.searchQuery} />
+        {status === 'idle' && <div></div>}
+        {status === 'pending' &&
+          <StyledLoader
+            type="BallTriangle"
+            color="#000"
+            height={50}
+            width={50} />
+        }
+        {status === 'rejected' && <h2>{error.message}</h2>}
+        {status === 'resolved' &&
+          <ImageGallery
+            images={images}
+            onImageClick={this.handleImageClick} />
+        }
+        {images && images.length > 11 && <Button onClick={this.handleLoadMoreBnt}/>}
+        {showModal &&
+          <Modal onClose={this.handleImageClick}>
+            <img src={selectedImage} alt="" onClick={this.handleImageClick}/>
+          </Modal>
+        }
       </Container>
     );
   }
--- src/Components/ImageGallery.js
+++ src/Components/ImageGallery.js
@@ -1,94 +1,16 @@
-import React, { Component } from 'react';
-import toast from 'react-hot-toast';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem';
-import imagesApi from '../services/images-api';
-import Modal from './Modal';
-import Button from './Button';
-import { ImageGalleryList, StyledLoader } from './ImagesFinder.styled';
+import { ImageGalleryList } from './ImagesFinder.styled';
 
-export default class ImageGallery extends Component {
-    state = {
-        images: null,
-        loading: false,
-        error: null,
-        showModal: false,
-        selectedImage: null,
-        page: 1,
-        status: 'idle',
-    };
+export default function ImageGallery ({ images, onImageClick}) {
+    return (
+            <div>
+                <ImageGalleryList>
+                    <ImageGalleryItem images={images} onClick={onImageClick}/>
+                </ImageGalleryList>
+            </div>
+        );
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.searchQuery !== this.props.searchQuery) {
-            this.setState({ page: 1, status: 'pending' });
-
-            imagesApi
-                .fetchImages(this.props.searchQuery, this.state.page)
-                .then(images => {
-                    if (!images.hits.length) {
-                        this.setState({ status: 'idle'});
-                        return toast.error(`No matches found`)
-                    }
-                    this.setState({ images: images.hits, status: 'resolved', page: this.state.page + 1 })
-                })
-                .catch(error => this.setState({ error, status: 'rejected'}))
-        }
-    }
-
-    handleImageClick = link => {
-        this.setState(({ showModal }) => ({
-            showModal: !showModal
-        }));
-        this.setState({ selectedImage: link });
-    }
-
-    handleLoadMoreBnt = () => {
-        this.setState({ page: this.state.page + 1 });
-
-        imagesApi
-            .fetchImages(this.props.searchQuery, this.state.page)
-            .then(images => {
-                this.setState(prevState => ({
-                    images: [...prevState.images, ...images.hits],
-                }));
-            })
-    }
-
-    render() {
-        const { images, error, status, showModal, selectedImage } = this.state;
-
-        if (status === 'idle') {
-            return <div></div>;
-        }
-        if (status === 'pending') {
-            return (
-                <StyledLoader
-                    type="BallTriangle"
-                    color="#000"
-                    height={50}
-                    width={50}
-                />
-            )
-        }
-        if (status === 'rejected') {
-            return <h2>{error.message}</h2>;
-        }
-        if (status === 'resolved') {
-            return (
-                <div>
-                    <ImageGalleryList>
-                        <ImageGalleryItem images={images} onClick={this.handleImageClick}/>
-                    </ImageGalleryList>
-                    {showModal &&
-                        <Modal onClose={this.handleImageClick}>
-                            <img src={selectedImage} alt="" onClick={this.handleImageClick}/>
-                        </Modal>
-                    }
-                    {images.length > 11 && <Button onClick={this.handleLoadMoreBnt}/>}
-                </div>
-            );
-        }
-    }
 }
 
 ImageGallery.propTypes = {
